947ad2c87d7b2a2733b4e427345ab7b9189ab216
dbahlburg <dominik.bahlburg@alumni.ubc.ca>
2018-09-25 19:57:43 +0200

    learn shiny, first reactive app

---
+++ reactive_app/app.R
@@ -0,0 +1,32 @@
+# Define UI ----
+ui <- fluidPage(
+  titlePanel("censusVis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+            helpText("Create demographic maps with",
+                     'information from the 2010 US Census.'),
+            selectInput("var", h4("Choose a variable to display"), 
+                       choices = list("Percent White", "Percent Black",
+                                      "Percent Hispanic", "Percent Asian"), selected = 'Percent Black'),
+            sliderInput("slider1", h3("Range of interest:"),
+                       min = 0, max = 100, value = c(0,100))
+     ),
+    mainPanel(textOutput("selected_var"),
+              textOutput('selected_range')
+              )
+  )
+)
+
+# Define server logic ----
+server <- function(input, output) {
+  
+  output$selected_var <- renderText({ 
+   paste("You have selected:",input$var)})
+  
+   output$selected_range <- renderText({ 
+     paste('You have chosen a range that goes from ', input$slider1[1],'to',input$slider1[2])})
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
